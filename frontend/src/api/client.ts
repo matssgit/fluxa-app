2e976071df70b4c844a571cb150cdcb15bd78648
Matheus Santana <100173811+matssgit@users.noreply.github.com>
2026-06-22 17:10:17 -0300

    upgrade client.ts

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-   baseURL: "http://localhost:3333",
+   baseURL: "https://finance-app-beta-2.onrender.com/",
    withCredentials: true, // ESSENCIAL para a API baseada em Cookies
 });
 
